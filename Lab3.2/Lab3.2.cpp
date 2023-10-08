1f360a170d3acc7376005a22532adf6afdd14fa6
Volodya22211 <Volodymyr.Baran.RI.2023@lpnu.ua>
2023-10-08 13:49:20 +0300

    Add project files.

---
+++ Lab3.2/Lab3.2.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace std;
+int main()
+{
+	double x; 
+	double a; 
+	double b; 
+	double c; 
+	double F; 
+	cout << "a = "; cin >> a;
+	cout << "b = "; cin >> b;
+	cout << "c = "; cin >> c;
+	cout << "x = "; cin >> x;
+
+
+	if (x+5<0 && c==0)
+		F = (1/(a*x))-b;
+	if (x + 5 > 0 && c != 0)
+		F = (x-a)/x;
+	if (!(x + 5 < 0 && c == 0) && !(x + 5 > 0 && c != 0))
+		F = (10*x)/(c-4);
+	cout << endl;
+	cout << "1) F = " << F << endl;
+
+	if (x + 5 < 0 && c == 0)
+		F = (1 / (a * x)) - b;
+	else
+		if (x + 5 > 0 && c != 0)
+			F = (x - a) / x;
+		else
+			F = (10 * x) / (c - 4);
+	cout << "2) F = " << F << endl;
+	cin.get();
+	return 0;
+}
